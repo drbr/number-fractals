745d4b4a4e39fd3232fa8e192cb8c4510cb0222f
Andrew Brandon <drbr@users.noreply.github.com>
2020-08-13 14:18:29 -0700

    Add a dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
 import React from "react";
 import "./App.css";
+import { generateUniqueId } from "./utils/generateId";
 
 export function App() {
   return (
     <div className="App">
-      <label id="carsLabel">Choose a car: </label>
-      <select name="cars" id="cars" aria-labelledby="carsLabel">
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
-      </select>
+      <Dropdown items={CarsItems} label="Select a car: " />
 
       <p>Can I see this?</p>
+    </div>
+  );
+}
+
+const CarsItems: DropdownProps["items"] = [
+  {
+    value: "volvo",
+    name: "Volvo",
+  },
+  {
+    value: "audi",
+    name: "Audi",
+  },
+  {
+    value: "ford",
+    name: "Ford",
+  },
+];
+
+export type DropdownProps = {
+  label: string;
+  items: ReadonlyArray<{
+    value: string;
+    name: string;
+  }>;
+};
+
+export function Dropdown(props: DropdownProps) {
+  const uniqueId = React.useMemo(() => generateUniqueId(), []);
+
+  return (
+    <div>
+      <label id={uniqueId + "-label"}>{props.label}</label>
+      <select id={uniqueId} aria-labelledby={uniqueId + "-label"}>
+        {props.items.map((item) => (
+          <option value={item.value} key={item.value}>
+            {item.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
