bbeb03572c14217ef67d297a95b26908d6a52224
Andrew Brandon <drbr@users.noreply.github.com>
2020-08-13 13:47:00 -0700

    Magically format code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <label for="cars">Choose a car:</label>
 
-<label for="cars">Choose a car:</label>
-
-<select name="cars" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select>
-
+      <select name="cars" id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
     </div>
   );
 }
