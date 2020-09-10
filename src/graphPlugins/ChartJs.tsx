72c0dad3d8876986ef7d5903ff864d59947f648c
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 15:54:49 -0700

    Add tooltip with word

--- src/graphPlugins/ChartJs.tsx
+++ src/graphPlugins/ChartJs.tsx
@@ -2,63 +2,56 @@ import * as React from "react";
 import { GraphProps } from "../plugins/GraphPlugins";
 import { Bar } from "react-chartjs-2";
 
-export const GraphWithChartJs: React.FC<GraphProps> = function GraphWithReactCharts({
-  values,
-}) {
-  const numberWords = values.map((d) => d.numberAsWords);
-  const numberValues = values.map((d) => d.value);
+export const GraphWithChartJs: React.FC<GraphProps> = React.memo(
+  function GraphWithReactCharts({ values }) {
+    const numberWords = values.map((d) => d.numberAsWords);
+    const numberValues = values.map((d) => d.value);
 
-  const chartData = {
-    labels: numberValues,
-    datasets: [
-      {
-        label: "Numeric value",
-        backgroundColor: "#A456CB",
-        data: numberValues,
-        minBarLength: 10,
-      },
-    ],
-  };
+    const chartData = {
+      labels: numberValues,
+      datasets: [
+        {
+          label: "Numeric value",
+          backgroundColor: "#A456CB",
+          data: numberValues,
+          minBarLength: 10,
+        },
+      ],
+    };
 
-  const axisOptions = {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
+    const axisOptions = {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    };
+
+    const animationOptions = {
+      duration: 0,
+    };
+
+    const tooltipOptions = {
+      callbacks: {
+        label: (tooltipItem: { index: number }) => {
+          const word = numberWords[tooltipItem.index];
+          return `${word}`;
         },
       },
-    ],
-  };
+    };
 
-  const animationOptions = {
-    duration: 0,
-  };
-
-  const tooltipOptions = {
-    callbacks: {
-      label: (
-        tooltipItem: { datasetIndex: number },
-        data: typeof chartData
-      ) => {
-        // var label = data.datasets[tooltipItem.datasetIndex].label || "";
-        // if (label) {
-        //   label += ": ";
-        // }
-        // label += Math.round(tooltipItem.yLabel * 100) / 100;
-        // return label;
-      },
-    },
-  };
-
-  return (
-    <Bar
-      data={chartData}
-      options={{
-        scales: axisOptions,
-        animation: animationOptions,
-        // tooltips: tooltipOptions,
-        maintainAspectRatio: false,
-      }}
-    />
-  );
-};
+    return (
+      <Bar
+        data={chartData}
+        options={{
+          scales: axisOptions,
+          animation: animationOptions,
+          tooltips: tooltipOptions,
+          maintainAspectRatio: false,
+        }}
+      />
+    );
+  }
+);
