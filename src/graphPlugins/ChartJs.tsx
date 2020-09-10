225fab8a2ba188d6c86f1cecf48bd934f7391712
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 14:09:55 -0700

    Try using chart.js without reading the documentation

--- src/graphPlugins/ChartJs.tsx
+++ src/graphPlugins/ChartJs.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { GraphItems } from "../plugins/GraphPlugins";
+import { Bar } from "react-chartjs-2";
 
 export const GraphWithChartJs: GraphItems = function GraphWithReactCharts(
   values
 ) {
-  return <>Chart.js support is not yet implemented!</>;
+  return (
+    <Bar
+      data={values}
+      width={100}
+      height={50}
+      options={{ maintainAspectRatio: false }}
+    />
+  );
 };
