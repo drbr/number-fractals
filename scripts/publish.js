57dd8ff4bcd384fa74572cbe46dd288039a68bce
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 16:02:55 -0700

    Add publish script

---
+++ scripts/publish.js
@@ -0,0 +1,8 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+
+const pathToPublish = path.join(process.cwd(), 'build');
+
+ghpages.publish(pathToPublish, {
+  message: 'Publish to gh-pages',
+});
