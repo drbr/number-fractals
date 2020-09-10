15567b101b53d76dd509016fed1fe365c962803a
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 15:34:49 -0700

    Add stub tooltip customization

--- src/graphPlugins/ChartJs.tsx
+++ src/graphPlugins/ChartJs.tsx
@@ -5,7 +5,7 @@ import { Bar } from "react-chartjs-2";
 export const GraphWithChartJs: GraphItems = function GraphWithReactCharts(
   data
 ) {
-  // const numberWords = data.map((d) => d.numberAsWords);
+  const numberWords = data.map((d) => d.numberAsWords);
   const values = data.map((d) => d.value);
 
   const chartData = {
@@ -20,22 +20,43 @@ export const GraphWithChartJs: GraphItems = function GraphWithReactCharts(
     ],
   };
 
+  const axisOptions = {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  };
+
+  const animationOptions = {
+    duration: 0,
+  };
+
+  const tooltipOptions = {
+    callbacks: {
+      label: (
+        tooltipItem: { datasetIndex: number },
+        data: typeof chartData
+      ) => {
+        // var label = data.datasets[tooltipItem.datasetIndex].label || "";
+        // if (label) {
+        //   label += ": ";
+        // }
+        // label += Math.round(tooltipItem.yLabel * 100) / 100;
+        // return label;
+      },
+    },
+  };
+
   return (
     <Bar
       data={chartData}
       options={{
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-        animation: {
-          duration: 0,
-        },
+        scales: axisOptions,
+        animation: animationOptions,
+        // tooltips: tooltipOptions,
         maintainAspectRatio: false,
       }}
     />
