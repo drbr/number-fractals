5a16c90be18bc84e40aee1ac8cc9ab3808dba5f4
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 14:05:01 -0700

    Bad news about react-charts

--- src/graphPlugins/ReactCharts.tsx
+++ src/graphPlugins/ReactCharts.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { GraphItems } from "../plugins/GraphPlugins";
 
-export const GraphWithReactCharts: GraphItems = function GraphWithReactCharts(
-  values
-) {
-  return <>react-charts support is not yet implemented!</>;
+export const GraphWithReactCharts: GraphItems = function GraphWithReactCharts() {
+  return <>react-charts, sadly, does not support Typescript!</>;
 };
