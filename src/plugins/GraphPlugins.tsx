9bd07eebef2c70dd649d5096f3bac316c5000c73
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 14:10:21 -0700

    Switch the order of the graph plugins

--- src/plugins/GraphPlugins.tsx
+++ src/plugins/GraphPlugins.tsx
@@ -18,13 +18,13 @@ export interface GraphPlugin extends BasePlugin {
 
 export const GraphPluginManager = new PluginManager<GraphPlugin>(
   {
-    registrationKey: "react-charts",
-    userVisibleName: "react-charts (TanStack)",
-    graphItems: GraphWithReactCharts,
-  },
-  {
     registrationKey: "react-charts-js",
     userVisibleName: "React Chart.js",
     graphItems: GraphWithChartJs,
+  },
+  {
+    registrationKey: "react-charts",
+    userVisibleName: "react-charts (TanStack)",
+    graphItems: GraphWithReactCharts,
   }
 );
