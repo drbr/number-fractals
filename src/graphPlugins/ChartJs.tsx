d72f748d71d131fa68143d396eb67861bc276405
Andrew Brandon <drbr@users.noreply.github.com>
2020-09-10 14:32:01 -0700

    Get a reasonable little chart

--- src/graphPlugins/ChartJs.tsx
+++ src/graphPlugins/ChartJs.tsx
@@ -3,14 +3,43 @@ import { GraphItems } from "../plugins/GraphPlugins";
 import { Bar } from "react-chartjs-2";
 
 export const GraphWithChartJs: GraphItems = function GraphWithReactCharts(
-  values
+  data
 ) {
+  // const numberWords = data.map((d) => d.numberAsWords);
+  const values = data.map((d) => d.value);
+
+  const chartData = {
+    labels: values,
+    datasets: [
+      {
+        label: "Numeric value",
+        backgroundColor: "#A456CB",
+        data: values,
+        minBarLength: 10,
+      },
+    ],
+  };
+
   return (
     <Bar
-      data={values}
-      width={100}
-      height={50}
-      options={{ maintainAspectRatio: false }}
+      data={chartData}
+      options={{
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+        animation: {
+          duration: 0,
+        },
+      }}
+      // width={100}
+      // height={50}
+      // options={{ maintainAspectRatio: false }}
     />
   );
 };
